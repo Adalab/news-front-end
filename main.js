ae7c19ecbb388c689f9d921d1b66f7fcd18e8c58
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2017-05-09 19:01:30 +0200

    add popularity chart

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var oReq = new XMLHttpRequest()
 oReq.addEventListener("load", reqListener)
-oReq.open("GET", "https://raw.githubusercontent.com/rladies/news/master/public/demo.json")
+  oReq.open("GET", "https://raw.githubusercontent.com/rladies/news/master/public/demo.json")
 oReq.send()
 
 // var title = document.getElementById('followers')
@@ -14,14 +14,35 @@ oReq.send()
 function reqListener () {
   var data = JSON.parse(this.responseText)
   var texthtml = ""
+  var titles=[]
+  var numVis=[]
   for (var i = 0; i < data.length; i++) {
+    titles.push(data[i].titulo);
+    numVis.push(data[i].visitas);
+
   	texthtml = texthtml + "<h1> "+ data[i].titulo +" </h1>"
   	texthtml = texthtml + "<p> "+ data[i].texto_noticia +" </p>"
   	texthtml = texthtml + "<p> "+ data[i].sentimiento +" </p>"
   	texthtml = texthtml + "<p> "+ data[i].visitas +" </p>"
   }
   document.getElementById('wrapper').innerHTML= texthtml
+  var ctx = document.getElementById("myChart");
 
+var data = {
+    labels: titles,
+    datasets: [
+        {
+            label: "Visitas",
+
+            borderWidth: 1,
+            data: numVis,
+        }
+            ]
+};
+var myBarChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: data
+    });
 
   //followersLabel.innerHTML = data.followers
 }
